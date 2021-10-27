e4b1d7863d105a01e2fee880ed5d7a9f516a0de6
Juan Pablo Escobedo Muñoz <61511918+JPEM19@users.noreply.github.com>
2021-10-27 15:46:47 -0500

    Update test_calculator.py
    Change some rules for flake8

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -1,15 +1,18 @@
 import calculator
 
-class TestCalculator:
+class TestCalculator: 
 
-    def test_addition(self):
+    def test_addition(self): 
         assert 4 == calculator.add(2,2)
 
-    def test_addition2(self):
+        
+    def test_addition2(self): 
         assert 5 == calculator.add(2,3)
 
-    def test_substraction(self):
+        
+    def test_substraction(self): 
         assert 2 == calculator.subtract(4,2)
 
-    def test_multiplication(self):
+        
+    def test_multiplication(self): 
         assert 100 == calculator.multiply(10,10)
